4d053ef38c9601875b3c878765702a011dee79fc
Jackson Denis <jackson.denis@live.com>
2022-07-23 10:15:17 -0300

    Encapsulamento

--- Banco.java
+++ Banco.java
@@ -1,6 +1,14 @@
 
 public class Banco {
 	
-	public String nome;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	
 }
--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,12 +1,25 @@
 
 public class ContaCorrente {
 	
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
 	public void sacar() {}
 	public void depositar() {}
 	public void transferir() {}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
 
 }
+
