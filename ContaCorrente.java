81add02d2e81f4f5cba639fc4312eed9f809d65e
Jackson Denis <jackson.denis@live.com>
2022-07-23 10:05:30 -0300

    Abstração

---
+++ ContaCorrente.java
@@ -0,0 +1,12 @@
+
+public class ContaCorrente {
+	
+	public int agencia;
+	public int numero;
+	public double saldo;
+	
+	public void sacar() {}
+	public void depositar() {}
+	public void transferir() {}
+
+}
---
+++ ContaPoupanca.java
@@ -0,0 +1,13 @@
+
+public class ContaPoupanca {
+	
+	public int agencia;
+	public int numero;	
+	public double saldo;
+	
+	public void sacar() {}
+	public void depositar() {}
+	public void transferir() {}
+
+
+}
